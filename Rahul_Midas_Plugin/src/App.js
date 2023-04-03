2af5db33e24f8c2a3ccc57de51d08ed55c808043
RahulMidas96 <rahul@midasit.com>
2023-04-03 20:25:38 +0900

    Update

--- Rahul_Midas_Plugin/src/App.js
+++ Rahul_Midas_Plugin/src/App.js
@@ -1,10 +1,15 @@
 import { ButtonGroup } from '@mui/material';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Appbar from './components/AppBar/appbar';
 import Grid from './components/Grid/Grid2';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+
+const baseUrl = "https://api-beta.midasit.com:443";
+const programType = 'civil';
+var mapiKey = "eyJ1ciI6IlJhaHVsTWlkYXM5NiIsInBnIjoiY2l2aWwiLCJjbiI6IlNUZVZDaW9SU0EifQ.d6366aff78ba1d787063b082a014569396454e67e5a01a6d4a4ce3dddc12b405"
 var Units;
 var Material;
 var Section;
@@ -40,11 +45,6 @@ var EMY
 var EMZ
 var StartOffset
 var EndOffset
-function StartApplication()
-{
-  getids()
-  
-}
 
 function getids(){
   Units=document.getElementById("Units").innerText;
@@ -54,7 +54,7 @@ function getids(){
   Number_Length_ofelement=document.getElementById("Number_Of_Elements").value;
   tabid1=document.getElementById("full-width-tab-0").tabIndex;
 
-  if(tabid1==0)
+  if(tabid1===0)
   {
     Coordinate_StartNode_Number=document.getElementById("Coordinate_Start_Number").value;
     Coordinate_StartNode_Coordinate=document.getElementById("Coordinate_Start_Coordinate").value;
@@ -107,6 +107,81 @@ function getids(){
   }
 
 }
+
+
+function StartApplication()
+{
+  getids();
+  checkMapiKey();
+createNode(1,5,0,0);
+createUnit("KN","MM","BTU","F")
+}
+
+async function checkMapiKey() {
+  // mapiKey를 QueryString으로부터 가져 옵니다.
+
+  const response = await fetch(`${baseUrl}/mapiKey/verify`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'MAPI-Key': mapiKey
+    }
+  });
+
+  // 응답 결과를 getnode-output id를 가진 DOM 객체에 전달합니다.
+  
+   console.log( JSON.stringify(await response.json(), null, 2));
+}
+
+async function createNode(ID,X,Y,Z){
+var newid=`${ID}`
+  var bd={
+    "Assign":{
+      [ID]: {
+         "X": X,
+         "Y": Y,
+         "Z": Z,
+     }
+  }
+  }
+
+ const response = await fetch(`${baseUrl}/${programType}/db/node`, {
+  method: "PUT",
+  headers: {
+    'Content-Type': 'application/json',
+    'MAPI-Key': mapiKey
+  },
+  body:JSON.stringify(bd)
+});
+
+}
+
+
+async function createUnit(Force, DIST, Heat, temp) {
+
+  const response = await fetch(`${baseUrl}/${programType}/db/unit`, {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+      'MAPI-Key': mapiKey
+    },
+    body: JSON.stringify(({
+      "Assign": {
+        "1": {
+          "FORCE": Force,
+          "DIST": DIST,
+          "HEAT": Heat,
+          "TEMPER": temp
+        }
+
+      }
+    }
+
+    ))
+
+  });
+}
+
 
 function App() {
   return (
