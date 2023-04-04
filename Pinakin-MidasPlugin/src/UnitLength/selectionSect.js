af4a70449231bcdf280c55e1b36fe1507a1a4865
pinakingore <pinakingore@gmail.com>
2023-04-04 17:49:09 +0900

    Getting Section from CIvil - Done

--- Pinakin-MidasPlugin/src/UnitLength/selectionSect.js
+++ Pinakin-MidasPlugin/src/UnitLength/selectionSect.js
@@ -3,32 +3,88 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+
+const get = async () => {
+  const res = await fetch('https://api-beta.midasit.com:443/civil/db/sect', {
+    headers: {
+      "Content-Type": "application/json",
+      // "MAPI-Key": "eyJ1ciI6ImxoeTAxMTgiLCJwZyI6ImNpdmlsIiwiY24iOiIwak9lUTJnYlJRIn0.d54292d340fc5e847d1f8220c3d316d8738c29ebc40ca5901f539ad6be44c66c"
+      "MAPI-Key": window.MAPIKey
+    }
+  })
+  if(res.ok){
+    const data = (await res.json())["SECT"];
+    return data;
+  }
+  else{
+    return "";
+  }
+}
+
+const defaultSect = [
+  {"200X300": 10},
+  {"300X300": 20},
+  {"300X500": 30},
+]
 
 export default function SelectSmall() {
-  const [age, setAge] = React.useState('');
+  const [sect, setSect] = React.useState('');
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setSect(event.target.value);
   };
 
+   //react State (useState)
+   const [sectList, setSectList] = React.useState([]);
+    //react Hook (useEffect)
+    React.useEffect(() => {
+      const initAsync = async () => {
+        const sectData = await get();
+        const temp = [];
+        if(sectData === "") {
+          sectData(defaultSect);
+        }
+        else {
+          let keys = Object.keys(sectData);
+          for ( let idx = 0; idx < keys.length; idx++ ) {
+            const sectId = keys[idx];
+            const TempObj = {};
+            TempObj[sectId] = sectData[sectId];
+            temp.push(TempObj)
+          }
+          console.log(temp);
+          setSectList(temp);
+        }
+      }
+  
+      //Async, Sync
+      initAsync();
+    }, []);
+
   return (
+    <Box sx={{ minWidth: 120, ml:1}}>
     <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
       {/* <InputLabel id="demo-select-small">Age</InputLabel> */}
       <Select sx={{minWidth: 120,ml:60,position:'absolute',mt:-4}} 
                             labelId="demo-select-small"
                             id="section"
-                            // value={age}
+                            value={sect}
                             // label="Age"
                             onChange={handleChange}
                             defaultValue={10}
                           >
-                            
-                            <MenuItem value={10}>200X300</MenuItem>
+                            {sectList.map((value, idx) => {
+                              const sectId = Object.keys(value)[0];
+                              const sectInfo = value[sectId];
+                              return <MenuItem key={idx} value={sectInfo}>{sectId}</MenuItem>
+                            })}
+                            {/* <MenuItem value={10}>200X300</MenuItem>
                             <MenuItem value={20}>300X300</MenuItem>
-                            <MenuItem value={30}>300X500</MenuItem>
+                            <MenuItem value={30}>300X500</MenuItem> */}
                           </Select>
     </FormControl>
-
+    </Box>
 
 
 
